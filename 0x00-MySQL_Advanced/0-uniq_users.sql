f12100ced45e41a6d8972734d83478026a054a12
Icelycoool <mohammedcali@proton.me>
2024-10-18 03:33:46 +0300

    2. Best band ever!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Creates 'users' table with the following attributes:
 -- id (int), email(string), name(string)
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    email VARCHAR(225) UNIQUE NOT NULL,
-    name VARCHAR(225)
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `email` VARCHAR(225) UNIQUE NOT NULL,
+    `name` VARCHAR(225),
+    PRIMARY KEY (`id`)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 -- Creates a 'users' table with the followng atrributes
 -- id(integer), email(string), name(string), country(enumeration)
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+    PRIMARY KEY(`id`)
 );
