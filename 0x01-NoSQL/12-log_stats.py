f1de4f7cf4f09649206b183642e54cc533c00149
Icelycoool <mohammedcali@proton.me>
2024-10-27 01:07:20 +0300

    Check fail correction

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,33 +3,36 @@
 
 from pymongo import MongoClient
 
-# Connect to the client
-client = MongoClient('mongodb://localhost:27017')
 
-# Access the logs
-log_collection = client.logs.nginx
+def stats_log():
+    """Displays some stats abount Nginx logs stored in MongoDB"""
+    # Connect to the client
+    client = MongoClient('mongodb://localhost:27017')
 
-# Get the total number of logs
-total_logs = log_collection.count_documents({})
+    # Access the logs
+    log_collection = client.logs.nginx
 
-# Count the logs of each method
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {
-    method: log_collection.count_documents({
-        "method": method}) for method in methods
-    }
+    # Get the total number of logs
+    total_logs = log_collection.count_documents({})
 
-# Status count for GET method
-get_status_count = log_collection.count_documents({
-    "method": "GET", "path": "/status"
-    })
+    # Count the logs of each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {
+        method: log_collection.count_documents({
+            "method": method}) for method in methods
+        }
 
-# Display information
-print(f"{total_logs} logs")
-print("Methods:")
-for method in methods:
-    print(f"\tmethod {method}: {method_counts[method]}")
-print(f"{get_status_count} status check")
+    # Status count for GET method
+    get_status_count = log_collection.count_documents({
+        "method": "GET", "path": "/status"
+        })
 
-# Close the MongoDB connection
-client.close()
+    # Display information
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{get_status_count} status check")
+
+    # Close the MongoDB connection
+    client.close()
