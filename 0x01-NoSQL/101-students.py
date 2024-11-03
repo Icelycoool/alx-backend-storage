5f646d1d1ea2abc2e4e5ebb3292b42650c86dcc0
Icelycoool <mohammedcali@proton.me>
2024-11-03 12:31:25 +0300

    14. Top students final

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,20 +4,23 @@
 
 def top_students(mongo_collection):
     """returns all students soretd by average score"""
-    students = mongo_collection.find()
-
-    results = []
-
-    for student in students:
-        scores = [topic['score'] for topic in student.get('topics', [])]
-        average_score = sum(scores) / len(scores) if scores else 0
-        student_with_average = {
-            '_id': student['_id'],
-            'name': student['name'],
-            'average_score': average_score,
-        }
-        results.append(student_with_average)
-    
-    results.sort(key=lambda x: x['avaerageScore'], reverse=True)
-
-    return results
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
