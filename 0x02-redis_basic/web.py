5ca4bcc109314112ef410237e5a8381557e89a3f
Icelycoool <mohammedcali@proton.me>
2024-11-04 19:15:56 +0300

    5. Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Web file"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+def track_get_page(fn: Callable) -> Callable:
+    """Decorator for get_page"""
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """Wrapper"""
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        respose = fn(url)
+        client.set(f'{url}', respose, 10)
+        return response
+    return wrapper
+
+
+def get_page(url: str) -> str:
+    """"Makes request to a particular url and gets the HTML content"""
+    resposne = requests.get(url)
+    return response.text
